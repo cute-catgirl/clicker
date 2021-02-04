d0241e83f97675fb7de4a46c31b5cea982a6441d
Boxit379 <66697315+Boxit379@users.noreply.github.com>
2021-02-04 09:48:36 -0700

    new stuff

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 var score = 0;
-function clickButton() {
-  score++;
+var totalclicks = 0;
+var upgradeCost = 100;
+var clickAmount = 1;
+function incrPoints() {
+  score += clickAmount;
+  totalclicks += clickAmount;
   document.getElementById("scoretext").innerHTML = "You have " + score + " points.";
-  if(score >= 100) {
-    console.log("cat")
+  if (score >= 50) {
+    document.getElementById("upgradeButton").style.display = "block";
+  }
+  if (score >= 200) {
+    document.getElementById("cpsButton").style.display = "block";
+  }
+}
+function upClick() {
+  if (score >= upgradeCost) {
+    score -= upgradeCost;
+    upgradeCost = upgradeCost * 2.5;
+    clickAmount = clickAmount * 2;
+    document.getElementById("scoretext").innerHTML = "You have " + score + " points.";
+    document.getElementById("upgradeButton").innerHTML = "upClick(" + upgradeCost + ");";
+    document.getElementById("clickButton").innerHTML = "incrPoints(" + clickAmount + ");";
   }
 }
